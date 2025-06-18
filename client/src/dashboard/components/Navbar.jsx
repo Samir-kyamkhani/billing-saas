898c83a4cdda0b2baf9a12280f0e6b71d294b320
Samir khan <samirkayamkhani.dev@gmail.com>
2025-06-18 11:09:16 +0530

    Add Links for navigation to home

--- client/src/dashboard/components/Navbar.jsx
+++ client/src/dashboard/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SearchInput from "./SearchInput";
 
 export default function Navbar() {
@@ -24,9 +24,9 @@ export default function Navbar() {
         <div className="relative hidden sm:block">
           <SearchInput className="w-full sm:w-64 px-3 py-1.5 rounded-md border border-gray-300 text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         </div>
-        <div className="rounded-full p-1">
+        <Link to={"/settings"} className="rounded-full p-1">
           <FaUserCircle className="text-3xl text-gray-600" />
-        </div>
+        </Link>
       </div>
     </header>
   );
